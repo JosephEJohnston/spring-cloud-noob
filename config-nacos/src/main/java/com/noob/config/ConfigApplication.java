9bbd17bee9e0553e504b90b27c0955e1ba3a2d97
Joseph Johnston <1193347519@qq.com>
2022-03-28 22:59:47 +0800

    36p-20:27

--- config-nacos/src/main/java/com/noob/config/ConfigApplication.java
+++ config-nacos/src/main/java/com/noob/config/ConfigApplication.java
@@ -10,7 +10,11 @@ public class ConfigApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(ConfigApplication.class, args);
 
+        // nacos 客户端每 10ms 去注册中心进行判断，根据 md5
+        // 配置不同则进行拉取
+
         String userName = applicationContext.getEnvironment().getProperty("user.name");
-        System.out.println(userName);
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.out.println(userName + ": " + userAge);
     }
 }
